ae22e144c87888a87f447160711690b4eb65523d
marciamillanao <marcia.millanao@gmail.com>
2018-06-15 16:23:30 -0400

    anadiendo html

---
+++ src/login.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyB0EP6V6GTJfr_s16c80Iysh3_pD-aVeAY",
+  authDomain: "red-social-7b68d.firebaseapp.com",
+  databaseURL: "https://red-social-7b68d.firebaseio.com",
+  projectId: "red-social-7b68d",
+  storageBucket: "red-social-7b68d.appspot.com",
+  messagingSenderId: "151412064408"
+};
+firebase.initializeApp(config);
