a050df83af5c3f170eac538a87fad037bee728a9
marciamillanao <marcia.millanao@gmail.com>
2018-06-21 14:06:20 -0400

    avances en login con feribase

--- src/login.js
+++ src/login.js
@@ -1,3 +1,8 @@
+window.onload = () => {
+  login();
+}
+
+function login() {
 const config = {
   apiKey: "AIzaSyB0EP6V6GTJfr_s16c80Iysh3_pD-aVeAY",
   authDomain: "red-social-7b68d.firebaseapp.com",
@@ -14,20 +19,42 @@ firebase.initializeApp(config);
   const btnSigUp = document.getElementById('btnSigUp');
   const btnLogout = document.getElementById('btnLogout');
 
-  btnLogin.addEventListener('click', e => {
-    const email = txtEmail.value;
-    const password = txtPassword.value;
-    const auth = firebase.auth();
+btnLogin.addEventListener('click', e => {
+
+  const email = txtEmail.value;
+  const password = txtPassword.value;
+  const auth = firebase.auth();
+  
 
     const promise = auth.signInWithEmailAndPassword(email, password);
     promise.catch(e => console.log(e.message));
   });
-
+  
   btnSigUp.addEventListener('click', e => {
-    const email = txtEmail.value;
-    const password = txtPassword.vale;
-    const auth = firebase.auth();
-
-    const promise = auth.createUserWithEmailAndPassword(email, password);
-    promise.catch(e => console.log(e.message));
+  const email = txtEmail.value;
+  const password = txtPassword.value;
+  const auth = firebase.auth();
+  
+  if (email.length == 0 || password.length == 0) {
+    alert("Los campos de la password no pueden quedar vacios");
+    return false;
+  }
+  
+  const promise = auth.createUserWithEmailAndPassword(email, password);
+  promise.catch(e => console.log(e.message));
   });
+  
+  btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+  });
+  
+  firebase.auth().onAuthStateChanged( firebaseUser => {
+    if(firebaseUser) {
+      console.log(firebaseUser);
+      btnLogout.classList.remove('hide');
+    }else{
+      console.log('no logueado');
+      btnLogout.classList.add('hide');
+    }
+  });
+}
